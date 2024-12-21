1fadb5cc1be815b5f487d09ebcf0ebc94c403e92
AlekseyB86 <avbelov86@mail.ru>
2024-12-21 18:14:06 +0300

    поправил код после ревью

--- src/main/java/faang/school/accountservice/handler/ErrorResponse.java
+++ src/main/java/faang/school/accountservice/handler/ErrorResponse.java
@@ -1,11 +1,13 @@
 package faang.school.accountservice.handler;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class ErrorResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
--- src/main/java/faang/school/accountservice/repository/OwnerRepository.java
+++ src/main/java/faang/school/accountservice/repository/OwnerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
